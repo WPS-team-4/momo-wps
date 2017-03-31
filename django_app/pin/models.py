cd918846398997f16c6ee7aeb894ebd83b61d5f5
Joonmo Kim <kizmo04@Joonmoui-MacBook-Pro.local>
2017-03-31 18:35:52 +0900

    pin 앱 생성/ Place, Map, Pin, HashTag, PinHashTag, Label, PinComment, PinPhoto 모델 구현 후 migration작성 / 스프린트 기간에 맞추기 위해 구현,  리팩토링 필요함.

---
+++ django_app/pin/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+
+from member.models import MomoUser
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    location = models.CharField(max_length=20)
+    place_id = models.CharField(max_length=100)
+
+
+class Map(models.Model):
+    author = models.ForeignKey(MomoUser)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)
+    is_visible = models.BooleanField(default=True)
+
+
+class Pin(models.Model):
+    author = models.ForeignKey(MomoUser)
+    place = models.ForeignKey(Place)
+    map = models.ForeignKey(Map)
+    name = models.CharField(max_length=100)
+    pin_color = models.CharField(max_length=10)
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)
+    is_visible = models.BooleanField(default=True)
+
+    def add_comment(self, user, content):
+        return self.pincomment_set.create(
+            author=user,
+            contnet=content
+        )
+
+    def add_photo(self, user, photo):
+        return self.pinphoto_set.create(
+            author=user,
+            photo=photo
+        )
+
+
+class HashTag(models.Model):
+    content = models.CharField(max_length=100, unique=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PinHashTag(models.Model):
+    hash_tag = models.ForeignKey(HashTag)
+    pin = models.ForeignKey(Pin)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = (
+            ('hash_tag', 'pin'),
+        )
+
+
+class Label(models.Model):
+    pin = models.ForeignKey(Pin)
+    color = models.CharField(max_length=10)
+    name = models.CharField(max_length=10)
+
+
+class PinComment(models.Model):
+    author = models.ForeignKey(MomoUser)
+    pin = models.ForeignKey(Pin)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PinPhoto(models.Model):
+    author = models.ForeignKey(MomoUser)
+    pin = models.ForeignKey(Pin)
+    photo = models.ImageField(upload_to='pin')
+    created_date = models.DateTimeField(auto_now_add=True)
