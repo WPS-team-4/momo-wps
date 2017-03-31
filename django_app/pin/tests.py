da1bb7eae9289ac549cada07936e999762e9a5f7
Joonmo Kim <kizmo04@Joonmoui-MacBook-Pro.local>
2017-03-31 19:09:24 +0900

    test - make_dummy_users 유틸 함수 작성

--- django_app/pin/tests.py
+++ django_app/pin/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def make_dummy_users():
+    users = []
+
+    for i in range(10):
+        user = MomoUser.objects.create_user(
+            username='username{}'.format(i + 1),
+            password='test_password'
+        )
+        users.append(user)
+    return users
+
+
+class MapTest(TestCase):
+    pass
