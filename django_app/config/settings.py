4c52e28bac0891dcb9a400facc6a762f0317e6cf
kizmo04 <kizmo04@gmail.com>
2017-03-31 18:31:07 +0900

    member 앱 생성, AUTH_USER_MODEL추가, MomoUser 모델 구현 후 migration작성

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -94,6 +94,8 @@ INSTALLED_APPS = [
 
     'corsheaders',
     'storages',
+
+    'member',
 ]
 
 MIDDLEWARE = [
@@ -149,6 +151,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'member.MomoUser'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
---
+++ django_app/member/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.db import models
+
+
+class MomoUserManager(BaseUserManager):
+    # def create_userhash(self, user):
+    #     hashed_email = pbkdf2_sha512.using(rounds=8000, salt_size=20).hash(user.email)[:40]
+    #     UserHash.objects.create(user=user,
+    #                             hashed_email=hashed_email + settings.SECRET_KEY)
+    #
+    #     send_auth_mail.send_activation_mail(user_email=user.email,
+    #                                         hashed_email=hashed_email)
+
+    def create_user(self, username, profile_img=None, password=None):
+        user = MomoUser(username=username, password=password)
+        user.set_password(password)
+        user.save()
+
+        # self.create_userhash(user)
+
+        return user
+
+    def create_superuser(self, username, password):
+        user = MomoUser(username=username, password=password)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+
+        user.save()
+
+        return user
+
+
+class MomoUser(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    profile_img = models.ImageField(blank=True, upload_to='member')
+    facebook_id = models.CharField(max_length=100, blank=True)
+    is_facebook = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = MomoUserManager()
