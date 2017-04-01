900bc3ca85572eedbb897b1ad27dfbf4890e16d7
sudokiz <get.kizmo@gmail.com>
2017-04-02 04:24:01 +0900

    Hash Tag Model Test 작성
    test_add_different_hash_tags_with_same_pin
    테스트 모드에서 test db를 앞선 테스트 케이스와 연달아 쓰기 때문에 테스트시에  usersname 키 중복 문제가 일어남.
    (IntegrityError: duplicate key value violates unique constraint testcase)
    class method로 setUpTestData를 만들어서 해결

--- django_app/pin/tests.py
+++ django_app/pin/tests.py
@@ -3,7 +3,7 @@ import random
 from django.test import TestCase
 
 from member.models import MomoUser
-from .models import Map, Pin, Place, HashTag
+from .models import Map, Pin, Place, HashTag, PinHashTag
 
 TEST_NAME = '피카츄', '라이츄', '파이리', '꼬부기', '버터플', '야도란', '피존투', '또가스'
 TEST_PLACE_NAME = '맛집', '주유소', '카페', '호텔', '도서관', 'PC방', '은행', '우체국'
@@ -114,3 +114,45 @@ class PinModelTest(TestCase):
             pins.append(pin)
 
         self.assertEqual(Pin.objects.filter(place=place).count(), 5)
+
+
+class HashTagModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = MomoUser.objects.create(
+            username='test_user{}{}'.format(1, 1),
+            password='test_password'
+        )
+
+        cls.place = Place.objects.create(
+            name='{}{}'.format(random.choice(TEST_NAME), random.choice(TEST_PLACE_NAME)),
+            address='{}나라 {}번지'.format(random.choice(TEST_NAME), 111),
+            location='{}.{}'.format(random.randrange(111, 999), random.randrange(111, 999)),
+        )
+
+        cls.map = Map.objects.create(
+            author=cls.user,
+            name='{} 지도'.format(random.choice(TEST_NAME)),
+            description='{}번째 지도입니다.'.format(1),
+        )
+
+    def test_add_different_hash_tags_with_same_pin(self):
+        tags = make_dummy_hash_tags(10)
+        # places = make_dummy_plces(1)
+        # maps = make_dummy_maps(1)
+        # users = make_dummy_users(1)
+
+        pin = Pin.objects.create(
+            author=self.user,
+            place=self.place,
+            map=self.map,
+            name='{}{}'.format(random.choice(TEST_NAME), random.choice(TEST_PLACE_NAME)),
+        )
+
+        for tag in tags:
+            PinHashTag.objects.create(
+                hash_tag=tag,
+                pin=pin
+            )
+        self.assertEqual(pin.pinhashtag_set.filter(pin=pin).count(), 10)
+        print(pin.pinhashtag_set.filter(pin__author=self.user))
