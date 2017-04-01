073a61a3c95d4a9badaa4b2320af441fb066bab7
sudokiz <get.kizmo@gmail.com>
2017-04-02 04:20:40 +0900

    Map Model Test 작성 - test_create_new_map

--- django_app/pin/tests.py
+++ django_app/pin/tests.py
@@ -71,3 +71,15 @@ def make_pins(num_place, num_map, num_user, num_pin):
         pins.append(pin)
     return pins
 
+
+class MapModelTest(TestCase):
+    def test_create_new_map(self):
+        user = make_dummy_users(1)[0]
+        name = random.choice(TEST_NAME)
+        Map.objects.create(
+            author=user,
+            name=name,
+            description='lasdjlfjskldjfl',
+        )
+        map = Map.objects.get(author=user)
+        self.assertEqual(map.name, name)
