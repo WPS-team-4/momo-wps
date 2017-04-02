743d552f822d49db348c56ab9f9080c8db90508b
anohk <anyoung.noh@gmail.com>
2017-04-02 21:46:35 +0900

    검색 템플릿, form 구현

--- django_app/place/urls/apis.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .. import apis
-urlpatterns = [
-    url(r'^search/$', apis.SearchPlaceView.as_view(), name='search-place'),
-    url(r'^resist/$', apis.ResistPlaceView.as_view(), name='resist-place'),
-]
