7562a663c6de90cba52b4821dd74f53490f6187b
sudokiz <get.kizmo@gmail.com>
2017-04-03 00:21:26 +0900

    url 수정

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from member import views
 
 urlpatterns = [
+    url(r'^profile/', views.ProfileViewAPI.as_view()),
     url(r'^login/', views.LoginAPI.as_view()),
 ]
