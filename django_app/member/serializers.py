4ca82255087dfff27be7158240b9d97f79060c98
sudokiz <get.kizmo@gmail.com>
2017-04-02 18:45:24 +0900

    User Serializer, Login Serializer 작성

---
+++ django_app/member/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'pk',
+            'username',
+            'password',
+            'profile_img',
+            'email',
+            'data_joined',
+            'facebook_id',
+            'is_facebook',
+            'is_superuser',
+            'is_staff',
+            'is_active',
+        )
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password',
+        )
