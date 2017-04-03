8bbf56fa24c0ca33afe8c0f9ff12c3ad6a6d2905
kizmo04 <kizmo04@gmail.com>
2017-04-03 14:07:48 +0900

    url 수정, UserProfileView API 구현

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from member import views
 
 urlpatterns = [
-    url(r'^profile/', views.ProfileViewAPI.as_view()),
+    url(r'^profile/', views.UserProfileViewAPI.as_view()),
     url(r'^login/', views.LoginAPI.as_view()),
 ]
