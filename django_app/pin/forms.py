388f2ed95a1c42c4458642b9e9d023042caa91ac
anohk <anyoung.noh@gmail.com>
2017-04-03 17:30:29 +0900

    Map 생성, Pin 생성 DB저장 완료

--- django_app/pin/forms.py
+++ django_app/pin/forms.py
@@ -2,8 +2,9 @@ from django import forms
 
 
 class CreateMapForm(forms.Form):
-    name = forms.CharField(label='map-name', max_length=100)
+    map_name = forms.CharField(label='map-name', max_length=100)
     description = forms.CharField(widget=forms.Textarea)
 
 
-
+class CreatePinForm(forms.Form):
+    pin_name = forms.CharField(label='pin-name', max_length=100)
--- django_app/place/urls/views.py
+++ django_app/place/urls/views.py
@@ -5,5 +5,5 @@ from place import views
 app_name = 'place'
 urlpatterns = [
     url(r'^$', views.search, name='search'),
-    url(r'^pin/$', views.create_place, name='pin-place')
+    url(r'^place/$', views.create_place, name='create-place')
 ]
