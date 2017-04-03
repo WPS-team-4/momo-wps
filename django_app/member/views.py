9fc86f4278bec26ba0ee068d960fa5f190cad3e8
sudokiz <get.kizmo@gmail.com>
2017-04-03 14:07:48 +0900

    url 수정, UserProfileView API 구현

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -3,6 +3,7 @@ import datetime
 from django.contrib.auth import authenticate
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_jwt import authentication
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
@@ -15,33 +16,15 @@ from member.serializers import UserSerializer, LoginSerializer
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
 
-class ProfileViewAPI(JSONWebTokenAPIView):
-    permission_classes = (permissions.IsAuthenticated,)
+class UserProfileViewAPI(JSONWebTokenAPIView):
+    permission_classes = (IsAuthenticated,)
     authentication_classes = (JSONWebTokenAuthentication,)
     serializer_class = VerifyJSONWebTokenSerializer
 
-    # def get_serializer(self, *args, **kwargs):
-    #     serializer_class = self.get_serializer_class()
-    #     kwargs['token'] = self.get_serializer_context()
-    #     return serializer_class(*args, **kwargs)
-
-    def get(self, request, *args, **kwargs):
-        # serializer = self.get_serializer(data=request.data)
-        # user = request.user
-        data = request.data('Authentication')
-        serializer = VerifyJSONWebTokenSerializer(data)
-        if serializer.is_valid():
-            user = request.user
-
-            # toke = serializer.object.get('token')
-            # response_data = jwt_response_payload_handler(token, user, request)
-            # response = Response(response_data)
-
-            # verify_jwt_token
-            user_serializer = UserSerializer(user)
-            return Response(user_serializer.data)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, format=None):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
 
 class LoginAPI(JSONWebTokenAPIView):
@@ -74,22 +57,3 @@ class LoginAPI(JSONWebTokenAPIView):
                 "error": ""
             }
         return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
-        # def post(self, request, *args, **kwargs):
-        #     serializer = self.get_serializer(data=request.data)
-        #
-        #     if serializer.is_valid():
-        #         user = serializer.object.get('user') or request.user
-        #         token = serializer.object.get('token')
-        #         response_data = jwt_response_payload_handler(token, user, request)
-        #         response = Response(response_data)
-        #         if api_settings.JWT_AUTH_COOKIE:
-        #             expiration = (datetime.utcnow() +
-        #                           api_settings.JWT_EXPIRATION_DELTA)
-        #             response.set_cookie(api_settings.JWT_AUTH_COOKIE,
-        #                                 response.data['token'],
-        #                                 expires=expiration,
-        #                                 httponly=True)
-        #         return response
-        #
-        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
