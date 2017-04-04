115e7459e38e37147ce4ec42d7e28440dc897848
anohk <anyoung.noh@gmail.com>
2017-04-04 20:27:29 +0900

    Pin API 구현

--- django_app/pin/serializers/pin.py
+++ django_app/pin/serializers/pin.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
+from map.models import Map
 from member.serializers import UserSerializer
 from pin.models import Pin
+from place.models import Place
 
 __all__ = (
     'PinSerializer'
@@ -11,11 +13,23 @@ __all__ = (
 class PinSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
 
+    # place = serializers.PrimaryKeyRelatedField(queryset=Place.objects.all(),
+    #                                            source='place.name')
+    # map = serializers.PrimaryKeyRelatedField(queryset=Map.objects.all(),
+    #                                          source='map.name')
+
     class Meta:
         model = Pin
         fields = (
             'pk',
             'author',
+            'name',
             'place',
-            'map'
+            'map',
+            'pin_color',
+            'is_private',
+        )
+        read_only_fields = (
+            'created_date',
+            'is_visible',
         )
