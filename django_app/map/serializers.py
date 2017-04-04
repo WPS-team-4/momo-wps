6b06fe1489b5795aa825e846740f8469583731b7
sudokiz <get.kizmo@gmail.com>
2017-04-04 23:44:09 +0900

    Map Serializer 작성

---
+++ django_app/map/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from map.models import Map
+# from member.serializers import UserSerializer
+
+
+class MapSerializer(serializers.ModelSerializer):
+    # author = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Map
+        fields = (
+            'pk',
+            'name',
+            'description',
+            'created_date',
+            'is_private',
+            'is_visible'
+        )
