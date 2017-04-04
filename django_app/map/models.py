33ca8e132d627e9c3a8756bd613c93d867533e61
Joonmo Kim <kizmo04@Joonmoui-MacBook-Pro.local>
2017-04-04 23:44:09 +0900

    Map Serializer 작성

--- django_app/map/models.py
+++ django_app/map/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from member.models import MomoUser
+
+
+class Map(models.Model):
+    author = models.ForeignKey(MomoUser)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)
+    is_visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.pk
