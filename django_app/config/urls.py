613920586f1413fee320ce86974efe8f9d0a86bc
anohk <anyoung.noh@gmail.com>
2017-04-04 17:42:48 +0900

    memeber - obtain auth 연결

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -16,13 +16,25 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+# API (JSONResponse)를 위한 urls
+from map.urls import apis as map_apis_urls
+from member.urls import apis as member_apis_urls
+from place.urls import apis as place_apis_urls
+
+# HttpRequest / HttpResponse를 위한 urls
 from place.urls import views as place_views_urls
-from . import views
+
+api_urlpatterns = [
+    url(r'^member/', include(member_apis_urls)),
+    url(r'^map/', include(map_apis_urls)),
+    url(r'^place/', include(place_apis_urls)),
+    # url(r'^pin/', include(pin_apis_urls)),
+]
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index'),
     url(r'^search/', include(place_views_urls)),
-    url(r'^pin/', include('pin.urls')),
+    # url(r'^pin/', include('pin.urls')),
+    url(r'^api/', include(api_urlpatterns, namespace='api'))
 
 ]
