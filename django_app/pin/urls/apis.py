c350a90ab7703007aba104160310e0e224dbedab
anohk <anyoung.noh@gmail.com>
2017-04-04 20:27:29 +0900

    Pin API 구현

--- django_app/pin/urls/apis.py
+++ django_app/pin/urls/apis.py
@@ -6,6 +6,4 @@ from .. import apis as views
 urlpatterns = [
     url(r'^$', views.PinList.as_view(), name='pin-list'),
     url(r'^(?P<pk>[0-9]+)/$', views.PinDetail.as_view(), name='pin-detail'),
-    # url(r'^pin/$', views.PinCreate.as_view(), name='pin-create'),
-    # url(r'^pin/(?P<pk>[0-9]+)/$', views.PinDestroy.as_view(), name='pin-destroy'),
 ]
