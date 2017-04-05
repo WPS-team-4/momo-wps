aa919cbd76db347079cc9360752bf4d574cdfff3
Joonmo Kim <kizmo04@Joonmoui-MacBook-Pro.local>
2017-04-05 22:23:27 +0900

    url name 수정

--- django_app/map/urls/apis.py
+++ django_app/map/urls/apis.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .. import apis as views
 
 urlpatterns = [
-    url(r'^$', views.MapList.as_view(), name='pin-list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.MapDetail.as_view(), name='pin-detail'),
+    url(r'^$', views.MapList.as_view(), name='map-list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.MapDetail.as_view(), name='map-detail'),
 ]
