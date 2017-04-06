b37f4455d1d0c8731174f5b1261681c4e6b15b2d
anohk <anyoung.noh@gmail.com>
2017-04-07 02:27:30 +0900

    comment와 photo모델을 Post 앱의 모델 내부로 이동

--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
-from pin.models import PinComment, PinPhoto, Pin
+from member.models import MomoUser
+from pin.models import Pin
+
+
+class PostComment(models.Model):
+    author = models.ForeignKey(MomoUser)
+    pin = models.ForeignKey(Pin)
+    content = models.TextField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class PostPhoto(models.Model):
+    author = models.ForeignKey(MomoUser)
+    pin = models.ForeignKey(Pin)
+    photo = models.ImageField(upload_to='post')
+    created_date = models.DateTimeField(auto_now_add=True)
 
 
 class Post(models.Model):
     pin = models.ForeignKey(Pin)
-    comment = models.ForeignKey(PinComment, blank=True)
-    photo = models.ForeignKey(PinPhoto, blank=True)
+    comment = models.ForeignKey(PostComment, blank=True)
+    photo = models.ForeignKey(PostPhoto, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
