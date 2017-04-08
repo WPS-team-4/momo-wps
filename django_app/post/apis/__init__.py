2977647e543530a55de1ef0ed86bd7c5f951f508
anohk <anyoung.noh@gmail.com>
2017-04-08 19:18:48 +0900

    post, post photo, post comment API 구현

--- django_app/post/apis/__init__.py
+++ django_app/post/apis/__init__.py
@@ -1,1 +1,3 @@
 from .post import *
+from .post_photo import *
+from .post_comment import *
--- django_app/post/serializers/post.py
+++ django_app/post/serializers/post.py
@@ -1,17 +1,28 @@
 from rest_framework import serializers
 
+from member.models import MomoUser
+from post.models import Post
+from post.serializers.post_comment import PostCommentSerializer
+from post.serializers.post_photo import PostPhotoSerializer
+
 __all__ = (
     'PostSerializer',
 )
 
 
 class PostSerializer(serializers.ModelSerializer):
+    # author = serializers.PrimaryKeyRelatedField(queryset=MomoUser.objects.all(), source='username')
+    # author = UserSerializer(read_only=True)
+    comment = PostCommentSerializer(read_only=True, many=True, source='postcomment_set')
+    photo_list = PostPhotoSerializer(read_only=True, many=True, source='postphoto_set')
+
     class Meta:
+        model = Post
         fields = (
+            'pk',
+            'pin',
             'author',
-            'comment',
-            'photo',
-        )
-        read_only_fields = (
             'created_date',
+            'comment',
+            'photo_list',
         )
