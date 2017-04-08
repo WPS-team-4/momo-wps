a4fd4ec394b7fa7f13b1a321c114f9b954db5cce
anohk <anyoung.noh@gmail.com>
2017-04-08 21:51:38 +0900

    postphoto, postcomment serializers 수정

--- django_app/post/serializers/post.py
+++ django_app/post/serializers/post.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from member.models import MomoUser
+from pin.models import Pin
 from post.models import Post
 from post.serializers.post_comment import PostCommentSerializer
 from post.serializers.post_photo import PostPhotoSerializer
@@ -13,8 +13,17 @@ __all__ = (
 class PostSerializer(serializers.ModelSerializer):
     # author = serializers.PrimaryKeyRelatedField(queryset=MomoUser.objects.all(), source='username')
     # author = UserSerializer(read_only=True)
-    comment = PostCommentSerializer(read_only=True, many=True, source='postcomment_set')
+    # comment = PostCommentSerializer(read_only=True, many=True, source='postcomment_set')
+    pin = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='name'
+    )
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
     photo_list = PostPhotoSerializer(read_only=True, many=True, source='postphoto_set')
+    comment_list = PostCommentSerializer(read_only=True, many=True, source='postcomment_set')
 
     class Meta:
         model = Post
@@ -23,6 +32,7 @@ class PostSerializer(serializers.ModelSerializer):
             'pin',
             'author',
             'created_date',
-            'comment',
+            # 'comment',
             'photo_list',
+            'comment_list',
         )
