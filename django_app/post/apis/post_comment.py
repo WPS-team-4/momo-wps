b01b57d5495a4645bb3deee71ca205a82ffc4a50
anohk <anyoung.noh@gmail.com>
2017-04-09 23:34:02 +0900

    PostCommentCreate API의 queryset을 오버라이드함, uri에서 post id를 받아와서 queryset에 적용

--- django_app/post/apis/post_comment.py
+++ django_app/post/apis/post_comment.py
@@ -5,14 +5,13 @@ from post.serializers import PostCommentSerializer
 
 
 class PostCommentCreate(generics.CreateAPIView):
-    queryset = PostComment.objects.all()
     serializer_class = PostCommentSerializer
 
-    # def get_queryset(self):
-    #     queryset = PostComment.objects.all()
-    #     post = self.kwargs['post_id']
-    #     queryset = queryset.filter(postcomment__post=post)
-    #     return queryset
+    def get_queryset(self):
+        queryset = PostComment.objects.all()
+        post = self.kwargs['post_id']
+        queryset = queryset.filter(postcomment__post=post)
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(
