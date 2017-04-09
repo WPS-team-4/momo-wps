8d8fe433790ce67f2a8bf4a639eb970209e7f8c1
sudokiz <get.kizmo@gmail.com>
2017-04-10 04:33:47 +0900

    member follow 테스트

--- django_app/member/tests.py
+++ django_app/member/tests.py
@@ -1,6 +1,34 @@
 from django.test import TransactionTestCase
 
+from utils.tests import make_dummy_users
+
 
 class MomoUserModelTest(TransactionTestCase):
     def test_follow(self):
-        users = make_dummy_users()
+        users = make_dummy_users(10)
+        users[0].follow(users[1])
+        users[0].follow(users[2])
+        users[0].follow(users[3])
+
+        users[1].follow(users[3])
+        users[1].follow(users[2])
+
+        users[2].follow(users[3])
+
+        self.assertIn(users[1], users[0].following.all())
+        self.assertIn(users[2], users[0].following.all())
+        self.assertIn(users[3], users[0].following.all())
+
+        self.assertIn(users[2], users[1].following.all())
+        self.assertIn(users[3], users[1].following.all())
+
+        self.assertIn(users[3], users[2].following.all())
+
+        self.assertIn(users[0], users[3].follower_set.all())
+        self.assertIn(users[1], users[3].follower_set.all())
+        self.assertIn(users[2], users[3].follower_set.all())
+
+        self.assertIn(users[0], users[2].follower_set.all())
+        self.assertIn(users[1], users[2].follower_set.all())
+
+        self.assertIn(users[0], users[1].follower_set.all())
