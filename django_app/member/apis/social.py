0d22400b0c7affb8becc7ff82b9135ca23c66bc4
sudokiz <get.kizmo@gmail.com>
2017-04-13 21:35:14 +0900

    follow 수정중

--- django_app/member/apis/social.py
+++ django_app/member/apis/social.py
@@ -1,8 +1,9 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from member.models import MomoUser
+from member.models import RelationShip, MomoUser
 from member.serializers import RelationShipSerializer
 
 __all__ = (
@@ -16,21 +17,29 @@ class FollowAPI(APIView):
 
     def post(self, request, *args, **kwargs):
         from_user_pk = self.kwargs['pk']
-        print(from_user_pk)
-        # self.request.data['from_user'] = self.request.user
-        # self.request.data['to_user'] = MomoUser.objects.get(id=from_user_pk)
+        # print(from_user_pk)
+        self.request.data['from_user'] = self.request.user.pk
+        self.request.data['to_user'] = from_user_pk
         # from_user = MomoUser.objects.get(id=from_user_pk)
         # to_user = self.request.user
         # print(self.request.data.get('from_user'))
         # print(self.request.data.get('to_user'))
         # print(self.request.data)
-        data = {
-            'from_user': MomoUser.objects.get(id=from_user_pk),
-            'to_user': self.request.user
-        }
-        serializer = RelationShipSerializer(data=data)
+        # data = {}
+        # data['from_user'] = MomoUser.objects.get(id=from_user_pk),
+        # data['to_user'] = self.request.user
+        # serializer = RelationShipSerializer(data=data)
         # serial_data = serializer(to_user=self.request.user, from_user=MomoUser.objects.get(id=from_user_pk))
-        if serializer.is_valid():
-            serializer.save()
+        # if serializer.is_valid():
+        #     serializer.save()
 
-        return Response(serializer.data)
+        # return Response(serializer.data)
+        from_user = MomoUser.objects.get(id=from_user_pk)
+        relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=self.request.user)
+        # serializer = RelationShipSerializer(data=relation)
+        # serializer.is_valid()
+        if is_follow:
+            return Response({'follow', relation.to_user.username, relation.from_user.username}, status=status.HTTP_201_CREATED)
+        else:
+            relation.delete()
+            return Response({'unfollow'}, status=status.HTTP_200_OK)
