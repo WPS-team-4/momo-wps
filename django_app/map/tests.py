f71dfdde8c66b16d65f4a92c5ccd7b75d7b32628
anohk <anyoung.noh@gmail.com>
2017-04-14 02:37:36 +0900

    데이터 중복 상태

--- django_app/map/tests.py
+++ django_app/map/tests.py
@@ -1,5 +1,6 @@
 import random
 
+from django.contrib.auth import get_user_model
 from django.urls import NoReverseMatch
 from django.urls import resolve
 from django.urls import reverse
@@ -9,6 +10,7 @@ from rest_framework.test import APILiveServerTestCase
 from map.models import Map
 from utils.tests.testcase_api import APITestCaseAuthMixin
 
+User = get_user_model()
 
 class MapModelTest(APITestCaseAuthMixin, APILiveServerTestCase):
     def test_apis_url_exist(self):
@@ -21,8 +23,9 @@ class MapModelTest(APITestCaseAuthMixin, APILiveServerTestCase):
             self.fail(e)
 
     def test_map_create(self):
+        user = self.create_user_and_login(self.client)
 
-        response = self.create_map()
+        response = self.create_map(1)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -37,6 +40,10 @@ class MapModelTest(APITestCaseAuthMixin, APILiveServerTestCase):
         # response의 author 값 검사
         response_auth = response.data['author']
         self.assertIn('username', response_auth)
+
+        self.assertEqual(Map.objects.count(), 1)
+        map = Map.objects.first()
+        self.assertEqual(map.author.id, user.id)
         # print(response_auth)
 
     def test_cannot_map_create__not_authenticated(self):
@@ -48,10 +55,11 @@ class MapModelTest(APITestCaseAuthMixin, APILiveServerTestCase):
         self.assertEqual(Map.objects.exists(), False)
 
     def test_map_list(self):
+
         # map list 확인
         self.create_user_and_login(self.client)
         num = random.randrange(1, 10)
-        self.create_map(self, num)
+        self.create_map(num)
         url = reverse('api:map-list')
         response = self.client.get(url)
         print(response)
