4db0883bd4cd6bc7d89119442b8ed4ce413d8e4a
kizmo04 <kizmo04@gmail.com>
2017-04-13 21:50:59 +0900

    follow API 구현

--- django_app/member/apis/social.py
+++ django_app/member/apis/social.py
@@ -16,30 +16,11 @@ class FollowAPI(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
-        from_user_pk = self.kwargs['pk']
-        # print(from_user_pk)
-        self.request.data['from_user'] = self.request.user.pk
-        self.request.data['to_user'] = from_user_pk
-        # from_user = MomoUser.objects.get(id=from_user_pk)
-        # to_user = self.request.user
-        # print(self.request.data.get('from_user'))
-        # print(self.request.data.get('to_user'))
-        # print(self.request.data)
-        # data = {}
-        # data['from_user'] = MomoUser.objects.get(id=from_user_pk),
-        # data['to_user'] = self.request.user
-        # serializer = RelationShipSerializer(data=data)
-        # serial_data = serializer(to_user=self.request.user, from_user=MomoUser.objects.get(id=from_user_pk))
-        # if serializer.is_valid():
-        #     serializer.save()
-
-        # return Response(serializer.data)
-        from_user = MomoUser.objects.get(id=from_user_pk)
+        from_user = MomoUser.objects.get(id=self.kwargs['pk'])
         relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=self.request.user)
-        # serializer = RelationShipSerializer(data=relation)
-        # serializer.is_valid()
         if is_follow:
-            return Response({'follow', relation.to_user.username, relation.from_user.username}, status=status.HTTP_201_CREATED)
+            return Response({'follow', relation.to_user.username, relation.from_user.username},
+                            status=status.HTTP_201_CREATED)
         else:
             relation.delete()
-            return Response({'unfollow'}, status=status.HTTP_200_OK)
+            return Response({'{} unfollow {}'.format()}, status=status.HTTP_200_OK)
