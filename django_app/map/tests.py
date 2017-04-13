f6688b8009574e47c2867876d5b33506ab5c94b5
anohk <anyoung.noh@gmail.com>
2017-04-13 23:47:56 +0900

    인증되지 않은 사용자 map 생성 테스트

--- django_app/map/tests.py
+++ django_app/map/tests.py
@@ -1,16 +1,14 @@
-import random
-
-from django.test import TestCase
 from django.urls import NoReverseMatch
 from django.urls import resolve
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APILiveServerTestCase
 
 from map.models import Map
-from utils.tests.testcase import TEST_NAME, make_dummy_users
+from utils.tests.testcase_api import APITestCaseAuthMixin
 
 
-class MapModelTest(TestCase):
+class MapModelTest(APITestCaseAuthMixin, APILiveServerTestCase):
     def test_apis_url_exist(self):
         try:
             # 1. MapList에 대한 url view가 존재하는지 확인
@@ -20,35 +18,32 @@ class MapModelTest(TestCase):
         except NoReverseMatch as e:
             self.fail(e)
 
-    def create_map(self, num=1):
-        # Map을 생성하는 API주소를 reverse
-        url = reverse('api:map-list')
-        for i in range(num):
-            response = self.client.post(url)
-            if num == 1:
-                return response
-
     def test_map_create(self):
 
         response = self.create_map()
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        user = make_dummy_users(1)[0]
-        map_name = random.choice(TEST_NAME)
 
-        Map.objects.create(
-            author=user,
-            map_name=map_name,
-            description='map description test',
-        )
-        map = Map.objects.get(author=user)
-        self.assertEqual(map.map_name, map_name)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # response의 key값 검사
+        self.assertIn('author', response.data)
+        self.assertIn('map_name', response.data)
+        self.assertIn('description', response.data)
+        self.assertIn('created_date', response.data)
+        self.assertIn('is_private', response.data)
+        # print('response_data: {}'.format(response.data))
+
         # response의 author 값 검사
+        response_auth = response.data['author']
+        self.assertIn('username', response_auth)
+        # print(response_auth)
 
     def test_cannot_map_create__not_authenticated(self):
         # 인증되지 않은 사용자가 map을 create 할 수 있는지 확인
-        pass
+        url = reverse('api:post-list')
+        response = self.client.post(url)
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Map.objects.exists(), False)
 
     def test_map_list(self):
         # map list 확인
