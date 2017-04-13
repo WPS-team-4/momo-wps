c5400ae8e8f43510b39bdff6104b4dfcb6c251ae
kizmo04 <kizmo04@gmail.com>
2017-04-14 03:53:16 +0900

    follow 수정중

--- django_app/member/apis/social.py
+++ django_app/member/apis/social.py
@@ -16,11 +16,18 @@ class FollowAPI(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
-        from_user = MomoUser.objects.get(id=self.kwargs['pk'])
-        relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=self.request.user)
+        to_user = MomoUser.objects.get(id=self.kwargs['pk'])
+        from_user = self.request.user
+        relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=to_user)
+        print(relation)
         if is_follow:
-            return Response({'follow', relation.to_user.username, relation.from_user.username},
+            return Response({"from_user": from_user.pk,
+                             "to_user": to_user.pk,
+                             "status": 'from_user{} follow to_user{}'.format(from_user.pk, to_user.pk)},
                             status=status.HTTP_201_CREATED)
         else:
             relation.delete()
-            return Response({'{} unfollow {}'.format()}, status=status.HTTP_200_OK)
+            return Response({"from_user": from_user.pk,
+                             "to_user": to_user.pk,
+                             "status": 'from_user{} unfollow to_user{}'.format(from_user.pk, to_user.pk)},
+                            status=status.HTTP_200_OK)
