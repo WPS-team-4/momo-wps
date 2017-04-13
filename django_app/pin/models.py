b02a044bd916afc0b5163b6dffc59ba2346e9abb
anohk <anyoung.noh@gmail.com>
2017-04-13 17:51:29 +0900

    pin model, serializer author 수정

--- django_app/pin/models.py
+++ django_app/pin/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 from map.models import Map
-from member.models import MomoUser
 from place.models import Place
 
 
 class Pin(models.Model):
-    author = models.ForeignKey(MomoUser)
     place = models.ForeignKey(Place)
     map = models.ForeignKey(Map)
     name = models.CharField(max_length=100)
@@ -15,21 +13,6 @@ class Pin(models.Model):
     is_private = models.BooleanField(default=False)
     is_visible = models.BooleanField(default=True)
 
-    # def add_comment(self, user, content):
-    #     return self.postcomment_set.create(
-    #         author=user,
-    #         contnet=content
-    #     )
-    #
-    # def add_photo(self, user, photo):
-    #     return self.postphoto_set.create(
-    #         author=user,
-    #         photo=photo
-    #     )
-
-    # def __str__(self):
-    #     return 'user({}) place({}) map({}) pin_name({})'.format(
-    #         self.author_id, self.place_id, self.map_id, self.name)
     def __str__(self):
         return 'pin: {}'.format(
             self.pk
--- django_app/pin/serializers/pin.py
+++ django_app/pin/serializers/pin.py
@@ -10,9 +10,21 @@ __all__ = (
 
 class PinSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
+        source='map.author',
+        slug_field='username',
+        read_only=True
     )
+    # author = UserSerializer(read_only=True, source='map__author')
+    # author = serializers.SlugRelatedField(
+    #     slug_field='username',
+    #     source='map__author',
+    #     read_only=True,
+    # )
+    # author = serializers.PrimaryKeyRelatedField(
+    #     source='map.author',
+    #     read_only=True,
+    # )
+
     post_list = PostSerializer(read_only=True, many=True, source='post_set')
 
     class Meta:
@@ -20,11 +32,11 @@ class PinSerializer(serializers.ModelSerializer):
         fields = (
             'pk',
             'author',
+            'map',
             'name',
             'place',
             'map',
             'pin_color',
             'created_date',
             'post_list',
-
         )
