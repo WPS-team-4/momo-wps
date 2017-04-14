28bc6a14a9932ffa5fa1a9e0b0188d36ed042b44
sudokiz <get.kizmo@gmail.com>
2017-04-14 22:48:46 +0900

    model 수정

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -32,7 +32,6 @@ class MomoUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(blank=True)
     password = models.CharField(max_length=100)
     profile_img = models.ImageField(blank=True, upload_to='member')
-    # facebook_id = models.CharField(max_length=100, blank=True)
     is_facebook = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
     relation = models.ManyToManyField(
