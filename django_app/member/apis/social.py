a7c8a9e126135da8ebf1288b694198618d1e1407
sudokiz <get.kizmo@gmail.com>
2017-04-14 16:41:02 +0900

    follow api 수정

--- django_app/member/apis/social.py
+++ django_app/member/apis/social.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from member.models import RelationShip, MomoUser
+from member.models import MomoUser
 
 __all__ = (
     'FollowAPI',
@@ -17,16 +17,19 @@ class FollowAPI(APIView):
     def post(self, request, *args, **kwargs):
         to_user = MomoUser.objects.get(id=self.kwargs['pk'])
         from_user = self.request.user
-        relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=to_user)
-        print(relation)
-        if is_follow:
-            return Response({"from_user": from_user.pk,
-                             "to_user": to_user.pk,
-                             "status": 'from_user{} follow to_user{}'.format(from_user.pk, to_user.pk)},
-                            status=status.HTTP_201_CREATED)
-        else:
-            relation.delete()
+        # relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=to_user)
+        # print(relation)
+        is_following = to_user in list(from_user.following)
+
+        if is_following:
+            from_user.unfollow(to_user)
             return Response({"from_user": from_user.pk,
                              "to_user": to_user.pk,
                              "status": 'from_user{} unfollow to_user{}'.format(from_user.pk, to_user.pk)},
                             status=status.HTTP_200_OK)
+        else:
+            from_user.follow(to_user)
+            return Response({"from_user": from_user.pk,
+                             "to_user": to_user.pk,
+                             "status": 'from_user{} follow to_user{}'.format(from_user.pk, to_user.pk)},
+                            status=status.HTTP_201_CREATED)
