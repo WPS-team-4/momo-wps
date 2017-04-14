85cb1113b8818f71dd8d26a354352dccf2b5dc29
sudokiz <get.kizmo@gmail.com>
2017-04-14 22:13:04 +0900

    create user serializer 삭제

--- django_app/member/serializers/auth.py
+++ django_app/member/serializers/auth.py
@@ -4,7 +4,7 @@ from member.models import MomoUser
 
 __all__ = (
     'LoginSerializer',
-    'CreateUserSerializer',
+    # 'CreateUserSerializer',
 )
 
 
@@ -16,22 +16,21 @@ class LoginSerializer(serializers.ModelSerializer):
             'password',
         )
 
-
-class CreateUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MomoUser
-        fields = ('pk', 'email', 'username', 'password')
-        # extra_kwargs = {'password': {'write_only': True}}
-
-        read_only_fields = (
-            'pk',
-        )
-
-    def create(self, validated_data):
-        user = MomoUser.objects.create(
-            email=validated_data['email'],
-            username=validated_data['username']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+# class CreateUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MomoUser
+#         fields = ('pk', 'email', 'username', 'password')
+#         # extra_kwargs = {'password': {'write_only': True}}
+#
+#         read_only_fields = (
+#             'pk',
+#         )
+#
+#     def create(self, validated_data):
+#         user = MomoUser.objects.create(
+#             email=validated_data['email'],
+#             username=validated_data['username']
+#         )
+#         user.set_password(validated_data['password'])
+#         user.save()
+#         return user
