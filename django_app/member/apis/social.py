5cb9a951c1b592acbdf0da92baa9eadeefc48ee9
kizmo04 <kizmo04@gmail.com>
2017-04-14 18:10:58 +0900

    자기 자신을 follow할 경우 예외 처리

--- django_app/member/apis/social.py
+++ django_app/member/apis/social.py
@@ -1,4 +1,5 @@
 from rest_framework import status
+from rest_framework.exceptions import NotAcceptable
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,25 +12,25 @@ __all__ = (
 
 
 class FollowAPI(APIView):
-    # serializer_class = RelationShipSerializer
     permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
         to_user = MomoUser.objects.get(id=self.kwargs['pk'])
         from_user = self.request.user
-        # relation, is_follow = RelationShip.objects.get_or_create(from_user=from_user, to_user=to_user)
-        # print(relation)
-        is_following = to_user in list(from_user.following)
-
-        if is_following:
-            from_user.unfollow(to_user)
-            return Response({"from_user": from_user.pk,
-                             "to_user": to_user.pk,
-                             "status": "unfollow"},
-                            status=status.HTTP_200_OK)
+        if to_user == from_user:
+            raise NotAcceptable(detail="자기 자신은 follow할 수 없습니다")
         else:
-            from_user.follow(to_user)
-            return Response({"from_user": from_user.pk,
-                             "to_user": to_user.pk,
-                             "status": "follow"},
-                            status=status.HTTP_201_CREATED)
+            is_following = to_user in list(from_user.following)
+
+            if is_following:
+                from_user.unfollow(to_user)
+                return Response({"from_user": from_user.pk,
+                                 "to_user": to_user.pk,
+                                 "status": "unfollow"},
+                                status=status.HTTP_200_OK)
+            else:
+                from_user.follow(to_user)
+                return Response({"from_user": from_user.pk,
+                                 "to_user": to_user.pk,
+                                 "status": "follow"},
+                                status=status.HTTP_201_CREATED)
