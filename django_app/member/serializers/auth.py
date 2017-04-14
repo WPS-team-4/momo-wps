7f5d7d9147b76287edf73411db5732b734a35932
sudokiz <get.kizmo@gmail.com>
2017-04-14 23:37:10 +0900

    create user serializer 삭제

--- django_app/member/serializers/auth.py
+++ django_app/member/serializers/auth.py
@@ -14,22 +14,3 @@ class LoginSerializer(serializers.ModelSerializer):
             'username',
             'password',
         )
-
-# class CreateUserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = MomoUser
-#         fields = ('pk', 'email', 'username', 'password')
-#         # extra_kwargs = {'password': {'write_only': True}}
-#
-#         read_only_fields = (
-#             'pk',
-#         )
-#
-#     def create(self, validated_data):
-#         user = MomoUser.objects.create(
-#             email=validated_data['email'],
-#             username=validated_data['username']
-#         )
-#         user.set_password(validated_data['password'])
-#         user.save()
-#         return user
