d512bc75266d424ea758b1e7b7036be3fee431b3
kizmo04 <kizmo04@gmail.com>
2017-04-15 11:44:32 +0900

    메인화면용 지도 정렬 api 수정중

--- django_app/map/apis/map.py
+++ django_app/map/apis/map.py
@@ -1,10 +1,10 @@
+from django.db.models import Count
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.response import Response
 
 from map.models import Map
-from map.serializers import MapSerializer
 from map.serializers.map import MapDetailSerializer
 
 __all__ = (
@@ -15,19 +15,28 @@ __all__ = (
 
 class MapList(generics.ListCreateAPIView):
     queryset = Map.objects.all()
-    serializer_class = MapSerializer
+    serializer_class = MapDetailSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
     def get(self, request, *args, **kwargs):
-        if self.request.auth:
+        try:
             fields = request.query_params.get('fields', '')
             if fields is not '':
                 fields = fields.split(',')
                 if 'recent_updated' in fields:
                     queryset = Map.objects.all().order_by('-created_date')
+                elif 'recent_created' in fields:
+                    queryset = Map.objects.all().order_by('-created_date')
+                elif 'most_pins' in fields:
+                    maps = Map.objects.annotate(number_of_pins=Count('pin'))
+                    count_pins = []
+                    for map, i in enumerate(maps):
+                        map.number_of_pins
+
+
             else:
                 queryset = Map.objects.filter(author=self.request.user)
 
@@ -38,8 +47,8 @@ class MapList(generics.ListCreateAPIView):
 
             serializer = self.get_serializer(queryset, many=True)
             return Response(serializer.data)
-        else:
-            raise NotAuthenticated
+        except TypeError:
+            raise NotAuthenticated(detail='로그인 해주세요')
 
 
 class MapDetail(generics.RetrieveUpdateDestroyAPIView):
