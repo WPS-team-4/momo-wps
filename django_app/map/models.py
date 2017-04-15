91ecbff92c7114138b1826938b32709f6192ea79
anohk <anyoung.noh@gmail.com>
2017-04-15 23:38:29 +0900

    description에 blank, null 허용

--- django_app/map/models.py
+++ django_app/map/models.py
@@ -7,7 +7,7 @@ from member.models import MomoUser
 class Map(models.Model):
     author = models.ForeignKey(MomoUser)
     map_name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     is_private = models.BooleanField(default=False)
     is_visible = models.BooleanField(default=True)
