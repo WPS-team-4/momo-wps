fe238da39ffe8c43ba705be1214c8d44a1f34b26
sudokiz <get.kizmo@gmail.com>
2017-04-15 19:29:01 +0900

    user view api 수정 중

--- django_app/member/apis/profile.py
+++ django_app/member/apis/profile.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -27,3 +27,23 @@ class UserProfileViewAPI(RetrieveUpdateAPIView):
             fields = None
         serializer = UserSerializer(user, fields=fields)
         return Response(serializer.data)
+
+
+class MainListViewAPI(RetrieveAPIView):
+    queryset = MomoUser.objects.all()
+    permission_classes = (IsOwnerOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = UserSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        fields = request.query_params.get('fields', '')
+        if fields is not '':
+            fields = fields.split(',')
+            if 'most_follower' in fields:
+                queryset = MomoUser.objects.filter()
+            elif 'most_maps' in fields:
+                queryset = MomoUser.objects.filter()
+        else:
+            fields = None
+                queryset = self.get_queryset()
+
