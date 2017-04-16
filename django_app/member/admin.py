828e5639a5a8cd3a52f7a7ca5f992171ed231ee9
sudokiz <get.kizmo@gmail.com>
2017-04-16 23:22:42 +0900

    RelationShip admin페이지 추가

--- django_app/member/admin.py
+++ django_app/member/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from rest_framework.authtoken.admin import TokenAdmin
 
-from member.models import MomoUser
+from member.models import MomoUser, RelationShip
 
 admin.site.register(MomoUser)
+admin.site.register(RelationShip)
 TokenAdmin.raw_id_fields = ('user',)
