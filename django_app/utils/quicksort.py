161c2ecb2880a26c223d21218066e69ecb5cab5a
sudokiz <get.kizmo@gmail.com>
2017-04-16 18:42:00 +0900

    quicksort 함수구현

--- django_app/utils/quicksort.py
+++ django_app/utils/quicksort.py
@@ -1,45 +1,26 @@
-maps = [2, 3, 6, 4, 88, 4, 7, 5, 5, 8, 2, 10, 45, 98, 0, 53, 33, 21, 13]
-len = len(maps)
-low = []
-high = []
-same = []
-print(maps)
-print(len)
+def quicksort(list):
+    if len(list) <= 1:
+        return list
 
-copy_maps = maps.copy()
+    low = []
+    high = []
+    same = []
+    pivot = list[len(list) // 2]
 
+    print('pick pivot:{}'.format(pivot))
+    for val in list:
+        print('list에서 {}을 선택'.format(val))
+        if pivot < val:
+            print('val{}을 high에 추가 // 현재 list: {}'.format(val, list))
+            high.append(val)
+            print('현재 high값:{}'.format(high))
+        elif pivot > val:
+            print('val{}을 low에 추가 // 현재 list: {}'.format(val, list))
+            low.append(val)
+            print('현재 low값:{}'.format(low))
+        elif pivot == val:
+            print('val{}을 same에 추가 // 현재 list: {}'.format(val, list))
+            same.append(val)
 
-pivot = maps[len // 2]
-print(pivot)
-maps[len // 2], maps[len - 1] = maps[len - 1], maps[len // 2]
-print(maps)
-print(pivot)
-
-print('pick pivot:{}'.format(pivot))
-for index, map in enumerate(maps):
-    # map = copy_maps.pop(index)
-    print('maps에서 {}번째 map:{}을 선택'.format(index, map))
-    if pivot < map:
-        print('map{}을 high에 추가 // 현재 maps: {}'.format(map, maps))
-        high.append(map)
-        print('현재 high값:{}'.format(high))
-
-    elif pivot > map:
-        # print('maps에서 {}번째 map{}을 선택'.format(index, map))
-        # map_in = copy_maps.pop(index)
-        print('map{}을 low에 추가 // 현재 maps: {}'.format(map, maps))
-        low.append(map)
-        print('현재 low값:{}'.format(low))
-    elif pivot == map:
-        print('map{}을 same에 추가 // 현재 maps: {}'.format(map, maps))
-        same.append(map)
-
-    print('최종 low:{} // 최종 same:{} // 최종 high:{}'.format(low, same, high))
-
-
-maps = low.append(same).append(high)
-# maps.append(pivot)
-# maps.append(high)
-print(maps)
-print(len(maps))
-    # low.append(pivot)
+        print('최종 low:{} // 최종 same:{} // 최종 high:{}'.format(low, same, high))
+    return quicksort(low) + same + quicksort(high)
