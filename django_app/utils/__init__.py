843f9d4dd9cb43a7a6779414efa4244afead5a1e
sudokiz <get.kizmo@gmail.com>
2017-04-16 18:43:50 +0900

    quicksort 유틸 함수 구현

--- django_app/utils/__init__.py
+++ django_app/utils/__init__.py
@@ -2,3 +2,4 @@ from .exceptions import *
 from .permissions import *
 from .serializer import *
 from .tests import *
+from .quicksort import *
--- django_app/utils/quicksort.py
+++ django_app/utils/quicksort.py
@@ -1,3 +1,8 @@
+__all__ = (
+    'quicksort',
+)
+
+
 def quicksort(list):
     if len(list) <= 1:
         return list
@@ -6,21 +11,12 @@ def quicksort(list):
     high = []
     same = []
     pivot = list[len(list) // 2]
-
-    print('pick pivot:{}'.format(pivot))
     for val in list:
-        print('list에서 {}을 선택'.format(val))
         if pivot < val:
-            print('val{}을 high에 추가 // 현재 list: {}'.format(val, list))
             high.append(val)
-            print('현재 high값:{}'.format(high))
         elif pivot > val:
-            print('val{}을 low에 추가 // 현재 list: {}'.format(val, list))
             low.append(val)
-            print('현재 low값:{}'.format(low))
         elif pivot == val:
-            print('val{}을 same에 추가 // 현재 list: {}'.format(val, list))
             same.append(val)
 
-        print('최종 low:{} // 최종 same:{} // 최종 high:{}'.format(low, same, high))
     return quicksort(low) + same + quicksort(high)
