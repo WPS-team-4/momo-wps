cdd75e6c80cbbebe55b28c166bdb0c6af3d24020
sudokiz <get.kizmo@gmail.com>
2017-04-16 22:32:50 +0900

    UserAPI url 연결

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from .. import apis as views
 
 urlpatterns = [
+    url(r'^$', views.UserAPI.as_view()),
     url(r'^(?P<pk>[0-9]+)/', views.UserDetailAPI.as_view()),
     url(r'^login/', views.LoginAPI.as_view()),
     url(r'^logout/', views.LogoutAPI.as_view()),
