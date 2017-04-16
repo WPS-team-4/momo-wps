26deccd5a31a3b6101b4500158314ed0e68c5359
anohk <anyoung.noh@gmail.com>
2017-04-17 02:36:06 +0900

    place 데이터가 들어오는 종류에 따라 place 생성 분리중

--- django_app/pin/apis/pin.py
+++ django_app/pin/apis/pin.py
@@ -10,6 +10,7 @@ from map.models import Map
 from pin.models import Pin
 from pin.serializers.pin import PinSerializer, PinCreateSerializer
 from place.models import Place
+from place.serializers import PlaceSerializer
 
 __all__ = (
     'PinList',
@@ -29,13 +30,27 @@ class PinList(generics.ListCreateAPIView):
         serializer.is_valid(raise_exception=True)
         place_data = data['place']
         pin_data = data['pin']
+
         place = self.place_data_to_object(place_data)
-        pin = self.create_pin_object(pin_data, place)
-        serializer = PinSerializer(pin)
+
+        map = Map.objects.get(pk=pin_data['map'])
+        # pin = self.create_pin_object(pin_data, place)
+
+        # pin = serializer.save()
+        serializer = PinSerializer(data=pin_data, place=place, map=map)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def place_data_to_object(self, data):
+
+
+        # place data에 모든 데이터가 들어있는 경우
+        serializer = PlaceSerializer(data=data)
+        serializer.is_valid()
+
+        # place에  latlng만 있는 경우
+
+        # latlng 만 받은 경우
         lat = data['lat']
         lng = data['lng']
         url = 'https://maps.googleapis.com/maps/api/geocode/json?'
@@ -56,15 +71,15 @@ class PinList(generics.ListCreateAPIView):
         place, _ = Place.objects.get_or_create(place_id=place_id, defaults=defaults)
         return place
 
-    def create_pin_object(self, data, place):
-        map = Map.objects.get(pk=data['map'])
-        pin = Pin.objects.create(
-            place=place,
-            map=map,
-            pin_name=data['pin_name'],
-            pin_color=data['pin_color']
-        )
-        return pin
+    # def create_pin_object(self, data, place):
+    #     map = Map.objects.get(pk=data['map'])
+    #     pin = Pin.objects.create(
+    #         place=place,
+    #         map=map,
+    #         pin_name=data['pin_name'],
+    #         pin_color=data['pin_color']
+    #     )
+    #     return pin
 
 
 class PinDetail(generics.RetrieveUpdateDestroyAPIView):
