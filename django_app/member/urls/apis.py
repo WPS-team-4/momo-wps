e5166dab409ecb851235c6891cee505ec653a198
sudokiz <get.kizmo@gmail.com>
2017-04-16 21:51:30 +0900

    UserProfileViewAPI 를 UserDetailAPI로 변경

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .. import apis as views
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/', views.UserProfileViewAPI.as_view()),
+    url(r'^(?P<pk>[0-9]+)/', views.UserDetailAPI.as_view()),
     url(r'^login/', views.LoginAPI.as_view()),
     url(r'^logout/', views.LogoutAPI.as_view()),
     url(r'^signup/', views.SignUpAPI.as_view()),
