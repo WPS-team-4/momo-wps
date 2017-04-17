256cf386ac37085569f025a9fbd6998eb64da5e2
sudokiz <get.kizmo@gmail.com>
2017-04-18 00:07:36 +0900

    member url 수정

--- django_app/member/urls/apis.py
+++ django_app/member/urls/apis.py
@@ -4,10 +4,10 @@ from .. import apis as views
 
 urlpatterns = [
     url(r'^$', views.UserAPI.as_view()),
-    url(r'^(?P<pk>[0-9]+)/', views.UserDetailAPI.as_view()),
-    url(r'^login/', views.LoginAPI.as_view()),
-    url(r'^logout/', views.LogoutAPI.as_view()),
-    url(r'^signup/', views.SignUpAPI.as_view()),
-    url(r'^fb/', views.FacebookLoginAPI.as_view()),
-    url(r'^(?P<pk>[0-9]+)/follow/', views.FollowAPI.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.UserDetailAPI.as_view()),
+    url(r'^login/$', views.LoginAPI.as_view()),
+    url(r'^logout/$', views.LogoutAPI.as_view()),
+    url(r'^signup/$', views.SignUpAPI.as_view()),
+    url(r'^fb/$', views.FacebookLoginAPI.as_view()),
+    url(r'^(?P<pk>[0-9]+)/follow/$', views.FollowAPI.as_view()),
 ]
