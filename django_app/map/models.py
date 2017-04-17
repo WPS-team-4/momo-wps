f4c2882f7944dd6593b10155c262c6d205131dcb
anohk <anyoung.noh@gmail.com>
2017-04-17 16:13:37 +0900

    Map 모델에 recent updated 필드 추가, map API parameter name 수정

--- django_app/map/models.py
+++ django_app/map/models.py
@@ -11,6 +11,7 @@ class Map(models.Model):
     map_name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     is_private = models.BooleanField(default=False)
     is_visible = models.BooleanField(default=True)
 
