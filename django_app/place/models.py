9af242bf0654b4683feb5560845eaebbe5b1741a
anohk <anyoung.noh@gmail.com>
2017-04-17 19:15:21 +0900

    Place 모델의 place_id 필드명을 googlepid 로 변경

--- django_app/place/models.py
+++ django_app/place/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Place(models.Model):
-    place_id = models.CharField(max_length=100, unique=True)
+    googlepid = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     lat = models.DecimalField(max_digits=20, decimal_places=16)
@@ -10,6 +10,6 @@ class Place(models.Model):
 
     def __str__(self):
         name = self.name
-        place_id = self.place_id
+        place_id = self.googlepid
 
         return '{} : {}'.format(name, place_id)
