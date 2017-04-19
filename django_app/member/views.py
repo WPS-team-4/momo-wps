87fe87dcc2bd51d3ea0c1a4ef73bcf62193dd294
sudokiz <get.kizmo@gmail.com>
2017-04-19 22:18:19 +0900

    email 내용 수정

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -4,7 +4,10 @@ except:
     from django.core.mail import send_mail
 
 
-def user_auth_mail(to_mail):
+def send_auth_mail(user):
     subject = "MOMO에 가입하신 것을 환영합니다."
     from_mail = "kizmo04@gmail.com"
-    send_mail(subject, 'Here is the message.', from_mail, [to_mail], fail_silently=False)
+    to_mail = user.email
+    url = 'http://eb-client.ap-northeast-2.elasticbeanstalk.com/api/member/activate/?key={}'.format(user.hash_username)
+    html_message = "<a href='{}'>여기를 눌러 계정을 활성화해주세요.</a>".format(url)
+    send_mail(subject, from_mail, [to_mail], fail_silently=False, html=html_message)
