7fd481003f1cbc39b6e52bcfd5b759113115cc5e
sudokiz <get.kizmo@gmail.com>
2017-04-19 20:31:00 +0900

    is_active default=False 로 변경

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -43,7 +43,7 @@ class MomoUser(AbstractBaseUser, PermissionsMixin):
 
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
