4b4d2af1f24b237700b8a99d9834acf1259bff5c
anohk <anyoung.noh@gmail.com>
2017-04-20 11:54:34 +0900

    post_headshot등록 및 createimage ondemand 를 False로 설정

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -79,12 +79,21 @@ else:
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # VersatileImageField
+VERSATILEIMAGEFIELD_SETTINGS = {
+    'create_images_on_demand': False,
+}
+
 VERSATILEIMAGEFIELD_RENDITION_KEY_SETS = {
     'user_headshot': [
         ('full_size', 'url'),
         ('thumbnail', 'thumbnail__120x120'),
         ('medium_square_crop', 'crop__400x400'),
     ],
+    'post_headshot': [
+        ('full_size', 'url'),
+        ('thumbnail', 'thumbnail__400x400'),
+        ('medium_square_crop', 'crop__800x800'),
+    ]
 }
 
 # Secret Key
