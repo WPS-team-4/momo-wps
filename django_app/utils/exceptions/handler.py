9f83536e72d300f4c2c99375dcb3f74fba105590
sudokiz <get.kizmo@gmail.com>
2017-04-20 21:22:48 +0900

    post create 시 예외처리 및 예외처리 핸들러 수정

--- django_app/utils/exceptions/handler.py
+++ django_app/utils/exceptions/handler.py
@@ -9,11 +9,11 @@ def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
     if response is not None:
         response.data = {}
-        errors = []
-        for field, value in response.data.items():
-            errors.append("{} : {}".format(field, " ".join(value)))
+        # errors = []
+        # for field, value in response.data.items():
+        #     errors.append("{} : {}".format(field, " ".join(value)))
 
-        response.data['errors'] = errors
+        # response.data['errors'] = errors
         response.data['status_code'] = '{} - {}'.format(response.status_code, response.status_text)
         response.data['detail'] = str(exc)
 
