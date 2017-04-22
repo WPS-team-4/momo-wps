abd17eac74fa79912e0a5c1af4224361a2e751dd
anohk <anyoung.noh@gmail.com>
2017-04-22 19:38:10 +0900

    pin API명 변경

--- django_app/pin/urls/apis.py
+++ django_app/pin/urls/apis.py
@@ -4,6 +4,6 @@ from .. import apis as views
 
 
 urlpatterns = [
-    url(r'^$', views.PinList.as_view(), name='pin-list'),
-    url(r'^(?P<pk>[0-9]+)/$', views.PinDetail.as_view(), name='pin-detail'),
+    url(r'^$', views.PinListAPI.as_view(), name='pin-list'),
+    url(r'^(?P<pk>[0-9]+)/$', views.PinDetailAPI.as_view(), name='pin-detail'),
 ]
