dab925cd35973294ce05dd2c36c7bf55c9ac737c
anohk <anyoung.noh@gmail.com>
2017-04-24 16:14:37 +0900

    이미지 파일 업로드 수정 photo, description의 여부에 따라 create 나눠놓음

--- django_app/post/apis/post.py
+++ django_app/post/apis/post.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
+from pin.models import Pin
 from post.models import Post
 from post.serializers.post import PostSerializer
 
@@ -20,15 +21,40 @@ class PostList(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = request.data
+        # print('request_data: {}'.format(data))
+        pin = Pin.objects.get(id=data.get('pin'))
         photo = data.get('photo')
+        file = request.FILES['photo']
         description = data.get('description')
+
+        print('pin: {}, photo: {}, decription: {}'.format(pin, photo, description))
         if photo or description:
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
+
+            if description and photo:
+                # print('*******')
+                post = Post.objects.create(
+                    pin=pin,
+                    photo=file,
+                    description=description
+                )
+            elif description is False:
+                # print('@@@@@@@@')
+                post = Post.objects.create(
+                    pin=pin,
+                    photo=file,
+                )
+            else:
+                # print('!!!!!!!!')
+                post = Post.objects.create(
+                    pin=pin,
+                    description=description
+                )
+
+            serializer = PostSerializer(post)
+            print('serializer.data: {}'.format(serializer.data))
+
             headers = self.get_success_headers(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
         else:
             raise ValidationError(detail="photo와 description 중 한가지 값은 꼭 입력해주세요.")
 
